2094880b4414cedf8f0c26ebddbdaf7e1cabc6ab
gerard.esteve <gesteve.12@gmail.com>
2025-05-19 07:31:42 +0200

    test fetch listener

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import 'jsr:@std/dotenv/load'
 
-import { Bot, webhookCallback } from 'https://deno.land/x/grammy/mod.ts'
+import { Bot } from 'https://deno.land/x/grammy/mod.ts'
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 
 const dev = Deno.env.get('ENV') === 'dev'
