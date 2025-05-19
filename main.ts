516854cec041315aa8e513c99594e8bf7bd703dc
gerard.esteve <gesteve.12@gmail.com>
2025-05-19 22:32:30 +0200

    add test cronjob

--- main.ts
+++ main.ts
@@ -4,3 +4,8 @@ import { startUp } from './src/bot/startup.ts'
 const bot = startUp(Deno.env.get('TELEGRAM_TOKEN')!)
 
 setupActions(bot)
+
+// Define cronjobs
+Deno.cron('Run once a minute', '* * * * *', () => {
+  console.log('Hello, cron!')
+})
