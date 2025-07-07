fe63551b4fbd710f0bd22506b14df6393468e021
gerard.esteve <gesteve.12@gmail.com>
2025-07-07 18:59:41 +0200

    testing deno deploy

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import { setupCommands } from './src/bot/commands.ts'
 import { startUp } from './src/bot/startup.ts'
 import { setupCronjobs } from './src/cronjobs/cronjobs.ts'
 
+console.log(Deno.env.get('PLATFORM'))
+
 const bot = startUp(Deno.env.get('TELEGRAM_TOKEN')!)
 setupCommands(bot)
 setupCronjobs(bot)
