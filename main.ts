328348aa5df59c82df483bb08b273d2222734e5f
gerard.esteve <gesteve.12@gmail.com>
2025-08-12 17:00:53 +0200

    test

--- main.ts
+++ main.ts
@@ -2,7 +2,11 @@ import { setupCommands } from './src/bot/commands.ts'
 import { startUp } from './src/bot/startup.ts'
 import { setupCronjobs } from './src/cronjobs/cronjobs.ts'
 
-const bot_lang = Deno.env.get('BOT_LANG') as 'cat' | 'es'
+// Debug environment variables
+console.log('All environment variables:', Object.fromEntries(Deno.env.toObject()))
+console.log('BOT_LANG raw value:', Deno.env.get('BOT_LANG'))
+
+const bot_lang = (Deno.env.get('BOT_LANG') as 'cat' | 'es') || 'cat'
 
 console.log(`Launching bot version: ${bot_lang}`)
 
