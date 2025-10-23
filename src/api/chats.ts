733394c7d233a108bb2e38be70e1f4dc178ffeb2
GerardEst <gesteve.12@gmail.com>
2025-10-23 23:29:44 +0200

    remove debug log

--- src/api/chats.ts
+++ src/api/chats.ts
@@ -4,15 +4,13 @@ import { supalog } from "./log.ts";
 export async function updateChartName(chatId: number, newName: string) {
   console.log(`Updating chat name ${chatId}`);
   try {
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from("chats")
       .upsert({ id: chatId, name: newName })
       .eq("id", chatId)
       .select();
 
     if (error) throw error;
-
-    supalog.debug(JSON.stringify(data));
   } catch (error) {
     console.error(error);
     supalog.error(JSON.stringify(error));
